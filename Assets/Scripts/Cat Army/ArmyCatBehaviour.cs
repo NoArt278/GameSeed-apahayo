d7c1f19af3fa0c7a567cbc5f634e7403d289867c
Addin Munawwar <91366027+moonawar@users.noreply.github.com>
2024-08-30 00:17:52 +0700

    feat: initialize cat army

---
+++ Assets/Scripts/Cat Army/ArmyCatBehaviour.cs
@@ -0,0 +1,27 @@
+using NaughtyAttributes;
+using UnityEngine;
+
+public class ArmyCatBehaviour : MonoBehaviour {
+    [Header("References")]
+    [SerializeField] private SpriteRenderer catRenderer;
+
+    [Header("Properties")]
+    [SerializeField] private float speed = 1f;
+
+    // public void Initialize()
+
+    #if UNITY_EDITOR
+    [Button]
+    private void LookAtCamera() {
+        if (Camera.main != null) {
+            Vector3 direction = Camera.main.transform.position - transform.position;
+            float xAngle = Mathf.Atan2(direction.y, -direction.z) * Mathf.Rad2Deg;
+            float yAngle = Mathf.Atan2(-direction.x, -direction.z) * Mathf.Rad2Deg;
+
+            catRenderer.transform.rotation = Quaternion.Euler(xAngle, yAngle, 0);
+        } else {
+            Debug.LogWarning("Main camera not found");
+        }
+    }
+    #endif
+}
--- Assets/Scripts/Cat Army/CatArmy.cs
+++ Assets/Scripts/Cat Army/CatArmy.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
+using NaughtyAttributes;
 using UnityEngine;
 
 public class CatArmy : MonoBehaviour
 {
-
+    [SerializeField] private Transform follow;
+    [ReadOnly] [SerializeField] private List<ArmyCatBehaviour> cats;
 }
