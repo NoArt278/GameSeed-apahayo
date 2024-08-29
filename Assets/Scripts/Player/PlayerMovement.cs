e29a9a09902a90000fe083739cd2bb3c00fd62c4
Nat10k <156871636+NoArt278@users.noreply.github.com>
2024-08-29 13:56:18 +0700

    feat : add cinemachine camera and rigidbody

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,27 +3,34 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     PlayerStats stats;
-    [SerializeField] private GameObject cat;
-    private GameObject currCat;
-    private float fireTimer, catLaunchSpeed = 8;
+    Rigidbody rb;
+    private int catAmmoCount;
+    private float fireTimer;
 
     private void Awake()
     {
         stats = GetComponent<PlayerStats>();
+        rb = GetComponent<Rigidbody>();
         fireTimer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
         Vector2 moveInput = InputContainer.playerInputs.Player.Move.ReadValue<Vector2>();
-        transform.position += stats.movementSpeed * Time.deltaTime * new Vector3(moveInput.x, 0, moveInput.y);
+        rb.velocity = stats.movementSpeed * new Vector3(moveInput.x, 0, moveInput.y);
         if (fireTimer > 0)
         {
             fireTimer -= Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Cat"))
+        {
+            catAmmoCount++;
+            other.enabled = false;
         }
     }
 }
