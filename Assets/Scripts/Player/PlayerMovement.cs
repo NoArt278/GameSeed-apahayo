5dea30c46315e8548582377b693219a12c8d89c4
Nat10k <156871636+NoArt278@users.noreply.github.com>
2024-08-29 15:40:42 +0700

    feat : hide mechanic

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,21 +1,26 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    PlayerStats stats;
-    Rigidbody rb;
+    private PlayerStats stats;
+    private Rigidbody rb;
+    private CapsuleCollider capsuleCollider;
+    private MeshRenderer mr;
     private int catAmmoCount;
     private const float maxStamina = 100, staminaDrainRate = 50, staminaFillRate = 10, minSprintStamina = 30;
-    private bool isSprinting = false;
+    private bool isSprinting = false, canHide = false, isHiding = false, justHid = false, canMove = true;
     private float fireTimer, stamina, moveSpeed;
-    [SerializeField] private TMP_Text staminaText, catCountText;
+    [SerializeField] private TMP_Text staminaText, catCountText, hideText;
 
     private void Awake()
     {
         stats = GetComponent<PlayerStats>();
         rb = GetComponent<Rigidbody>();
+        mr = GetComponent<MeshRenderer>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
         fireTimer = 0;
         stamina = maxStamina;
         moveSpeed = stats.walkSpeed;
@@ -24,23 +29,28 @@ public class PlayerMovement : MonoBehaviour
     private void Start()
     {
         InputContainer.playerInputs.Player.Sprint.performed += StartSprint;
-        InputContainer.playerInputs.Player.Sprint.canceled += StopSprint;
+        InputContainer.playerInputs.Player.Sprint.canceled += StopSprintInput;
+        InputContainer.playerInputs.Player.Interact.performed += Hide;
     }
 
     private void OnDisable()
     {
         InputContainer.playerInputs.Player.Sprint.performed -= StartSprint;
-        InputContainer.playerInputs.Player.Sprint.canceled -= StopSprint;
+        InputContainer.playerInputs.Player.Sprint.canceled -= StopSprintInput;
+        InputContainer.playerInputs.Player.Interact.performed -= Hide;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveInput = InputContainer.playerInputs.Player.Move.ReadValue<Vector2>();
-        rb.velocity = moveSpeed * new Vector3(moveInput.x, 0, moveInput.y);
-        if (fireTimer > 0)
+        if (canMove)
         {
-            fireTimer -= Time.deltaTime;
+            Vector2 moveInput = InputContainer.playerInputs.Player.Move.ReadValue<Vector2>();
+            rb.velocity = moveSpeed * new Vector3(moveInput.x, 0, moveInput.y);
+        } else
+        {
+            rb.velocity = Vector3.zero;
+            StopSprint();
         }
 
         if (isSprinting)
@@ -48,8 +58,7 @@ public class PlayerMovement : MonoBehaviour
             stamina = Mathf.Max(stamina - Time.deltaTime * staminaDrainRate, 0);
             if (stamina == 0)
             {
-                isSprinting = false;
-                moveSpeed = stats.walkSpeed;
+                StopSprint();
             }
         }
         else
@@ -61,17 +70,52 @@ public class PlayerMovement : MonoBehaviour
 
     private void StartSprint(InputAction.CallbackContext ctx)
     {
-         if (stamina > minSprintStamina)
+         if (stamina > minSprintStamina && canMove)
         {
             isSprinting = true;
             moveSpeed = stats.sprintSpeed;
         }
     }
 
-    private void StopSprint(InputAction.CallbackContext ctx)
+    private void StopSprintInput(InputAction.CallbackContext ctx)
+    {
+        StopSprint();
+    }
+
+    private void StopSprint()
     {
         isSprinting = false;
         moveSpeed = stats.walkSpeed;
+    }
+
+    private void Hide(InputAction.CallbackContext ctx)
+    {
+        if (canHide && !isHiding && !justHid)
+        {
+            StopAllCoroutines();
+            StartCoroutine(HideDelay());
+            isHiding = true;
+            canMove = false;
+            capsuleCollider.enabled = false;
+            mr.enabled = false;
+            hideText.text = "Press E to unhide";
+        } else if (isHiding && !justHid)
+        {
+            StopAllCoroutines();
+            StartCoroutine(HideDelay());
+            isHiding = false;
+            canMove = true;
+            capsuleCollider.enabled = true;
+            mr.enabled = true;
+            hideText.text = "Press E to hide";
+        }
+    }
+
+    private IEnumerator HideDelay()
+    {
+        justHid = true;
+        yield return new WaitForSeconds(0.5f);
+        justHid = false;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -80,6 +124,19 @@ public class PlayerMovement : MonoBehaviour
         {
             catAmmoCount++;
             catCountText.text = "Cat : " + catAmmoCount.ToString();
+        } else if (other.CompareTag("Hide"))
+        {
+            canHide = true;
+            hideText.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Hide"))
+        {
+            canHide = false;
+            hideText.gameObject.SetActive(false);
         }
     }
 }
