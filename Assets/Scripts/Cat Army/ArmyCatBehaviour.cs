6e3d75bb075d2d20bf1d9244f435bac45d866104
Addin Munawwar <91366027+moonawar@users.noreply.github.com>
2024-08-30 03:05:59 +0700

    feat: army catss!!

--- Assets/Scripts/Cat Army/ArmyCatBehaviour.cs
+++ Assets/Scripts/Cat Army/ArmyCatBehaviour.cs
@@ -1,26 +1,65 @@
+using DG.Tweening;
 using NaughtyAttributes;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ArmyCatBehaviour : MonoBehaviour {
     [Header("References")]
     [SerializeField] private SpriteRenderer catRenderer;
 
     [Header("Properties")]
-    [SerializeField] private float followSpeed = 3f;
+    // [SerializeField] private float rotationSpeed = 5f;
     private Transform follow;
+    private NavMeshAgent agent;
+
+    private bool followTheLeader = true;
+    private Vector3 towardsFleePosition;
+    private Vector3 fleePosition;
+
+    private void Awake() {
+        agent = GetComponent<NavMeshAgent>();
+    }
 
     public void Initialize(Transform follow) {
         this.follow = follow;
     }
 
-    private void StickToGround() {
-        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, 1f)) {
-            transform.position = hit.point;
+    private void Update() {
+        if (followTheLeader) {
+            agent.SetDestination(follow.position);
+        } else {
+            towardsFleePosition = Vector3.MoveTowards(towardsFleePosition, fleePosition, 0.5f);
+            agent.SetDestination(towardsFleePosition);
         }
+
+        AlignOrientation();
     }
 
-    private void Update() {
-        StickToGround();
+    public void Flee() {
+        followTheLeader = false;
+
+        Vector3 fleeDirection = (transform.position - follow.position).normalized;
+        fleePosition = follow.position + 100f * agent.radius * fleeDirection;
+        towardsFleePosition = transform.position;
+
+        Debug.DrawLine(transform.position, fleePosition, Color.red, 5f);
+
+        agent.SetDestination(towardsFleePosition);
+
+        Sequence sequence = DOTween.Sequence();
+        sequence.AppendInterval(1.2f);
+        sequence.Append(catRenderer.DOFade(0, 0.4f));
+
+        sequence.Play();
+    }
+
+    private void AlignOrientation() {
+        // Vector3 direction = follow.position - transform.position;
+        // float targetYAngle = direction.x > 0 ? 0 : 180;
+
+        // transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, targetYAngle, 0), Time.deltaTime * 5);
+        // LookAtCamera();
+        catRenderer.flipX = agent.velocity.x < 0;
     }
 
     #if UNITY_EDITOR
--- Assets/Scripts/Cat Army/CatArmy.cs
+++ Assets/Scripts/Cat Army/CatArmy.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using NaughtyAttributes;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CatArmy : MonoBehaviour
 {
@@ -8,18 +9,63 @@ public class CatArmy : MonoBehaviour
     [SerializeField] private List<ArmyCatBehaviour> cats;
     [SerializeField] private GameObject armyCatPrefab;
 
+    private float catRadius;
+
     private void Awake() {
         foreach (ArmyCatBehaviour cat in cats) {
             cat.Initialize(follow);
         }
+
+        catRadius = armyCatPrefab.GetComponent<NavMeshAgent>().radius;
     }
     
     [Button]
     public void AddCat()
     {
         GameObject newCat = Instantiate(armyCatPrefab, transform);
+        newCat.transform.position = FindAppropriateLocation();
         ArmyCatBehaviour catBehaviour = newCat.GetComponent<ArmyCatBehaviour>();
         catBehaviour.Initialize(follow);
         cats.Add(catBehaviour);
+    }
+
+    public void FleeOuterCat() {
+        if (cats.Count == 0) return;
+
+        ArmyCatBehaviour outerCat = cats[0];
+        float maxDistance = Vector3.Distance(outerCat.transform.position, follow.position);
+        foreach (ArmyCatBehaviour cat in cats) {
+            float distance = Vector3.Distance(cat.transform.position, follow.position);
+            if (distance > maxDistance) {
+                outerCat = cat;
+                maxDistance = distance;
+            }
+        }
+
+        outerCat.Flee();
+        cats.Remove(outerCat);
+    }
+
+    private Vector3 FindAppropriateLocation()
+    {
+        if (cats.Count == 0)
+        {
+            return follow.position + 2 * catRadius * Random.insideUnitSphere;
+        }
+
+        Vector3 averagePosition = Vector3.zero;
+        foreach (ArmyCatBehaviour cat in cats)
+        {
+            averagePosition += cat.transform.position;
+        }
+        averagePosition /= cats.Count;
+
+        Vector3 followDirection = (follow.position - averagePosition).normalized;
+        if (followDirection == Vector3.zero)
+        {
+            followDirection = Random.insideUnitSphere;
+        }
+
+        return averagePosition + 2 * catRadius * followDirection;
     }
 }
