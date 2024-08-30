a9f29fb46273bf99a1b8feb6b3dfa0129cdf3608
Addin Munawwar <moonawar@protonmail.com>
2024-08-30 16:56:57 +0700

    chore: add cat state for cat detection (only detect when state == army)

--- Assets/Scripts/Cat/CatBehaviourManager.cs
+++ Assets/Scripts/Cat/CatBehaviourManager.cs
@@ -8,6 +8,9 @@ public class CatBehaviourManager : MonoBehaviour {
     private StrayCatBehaviour strayCatBehaviour;
     private HidingCatBehaviour hidingCatBehaviour;
 
+    public enum State { Stray, Army, Hiding }
+    public State CurrentState { get; private set; } = State.Stray;
+
     private void Awake() {
         armyCatBehaviour = GetComponent<ArmyCatBehaviour>();
         strayCatBehaviour = GetComponent<StrayCatBehaviour>();
@@ -20,17 +23,23 @@ public class CatBehaviourManager : MonoBehaviour {
         armyCatBehaviour.enabled = false;
         strayCatBehaviour.enabled = true;
         hidingCatBehaviour.enabled = false;
+
+        CurrentState = State.Stray;
     }
 
     public void BecomeArmyCat() {
         armyCatBehaviour.enabled = true;
         strayCatBehaviour.enabled = false;
         hidingCatBehaviour.enabled = false;
+
+        CurrentState = State.Army;
     }
 
     public void BecomeHidingCat() {
         armyCatBehaviour.enabled = false;
         strayCatBehaviour.enabled = false;
         hidingCatBehaviour.enabled = true;
+
+        CurrentState = State.Hiding;
     }
 }
