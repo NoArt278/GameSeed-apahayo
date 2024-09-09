684a1ec847e28d303cd3779afbcffdcae41a7f3b
Edia Zaki <13521141@mahasiswa.itb.ac.id>
2024-09-09 17:56:54 +0700

    feat: dog inner vision

--- Assets/Scripts/Dog/FieldOfViewEditor.cs
+++ Assets/Scripts/Dog/FieldOfViewEditor.cs
@@ -8,14 +8,14 @@ public class FieldOfViewEditor : Editor
     {
         FieldOfView fov = (FieldOfView)target;
         Handles.color = Color.white;
-        Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.Radius);
+        Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.outerRadius);
 
         Vector3 viewAngle01 = DirectionFromAngle(fov.transform.eulerAngles.y, -fov.Angle / 2);
         Vector3 viewAngle02 = DirectionFromAngle(fov.transform.eulerAngles.y, fov.Angle / 2);
 
         Handles.color = Color.yellow;
-        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngle01 * fov.Radius);
-        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngle02 * fov.Radius);
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngle01 * fov.outerRadius);
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngle02 * fov.outerRadius);
     }
 
     private Vector3 DirectionFromAngle(float eulerY, float degreeAngle)
